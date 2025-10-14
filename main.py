8335c08a963a78d66af8cc80c93a31c669b16fba
balibalouu <basilegamar11@gmail.com>
2025-10-14 09:35:25 +0200

    ajout class forme incomplet

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import pygame
 
 
+forme_possible=[["forme1"],["forme2"],["forme3"]]
+
+class Forme:
+    def __init__(self):
+        self.pos_x = 5
+        self.pos_y = 0
+        self.forme=random.choice(forme_possible)
+        
+    def decendre(self):
+        self.pos_y+=1
+    
+    def gauche(self):
+        if self.pos_x>0:
+            self.pos_x-=1
+    
+    def droit(self):
+        if self.pos_x<10:
+            self.pos_x+=1
+
 class Game:
     def __init__(self, rows=10, cols=10, cell_size=40):
         pygame.init()
@@ -37,6 +56,10 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.key == pygame.K_LEFT:
+                    self.Forme.gauche()
+                if event.key == pygame.K_RIGHT:
+                    self.Forme.droit
 
             self.screen.fill((0, 0, 0))
             self.draw_grid()
